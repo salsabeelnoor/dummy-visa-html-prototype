957ca395ea22f6fce2e4ad3f46f2e1a2fe2e0a73
Salsabeel Noor <salsabeelnoor@gmail.com>
2024-09-02 15:36:34 +0600

    banner nav complete

--- js/video.js
+++ js/video.js
@@ -1,32 +1,6 @@
-
-const video = document.getElementById('videoElement');
-const duration = 5; // The duration to play before reversing (in seconds)
-let playForward = true;
-
-video.currentTime = 0;
-video.play();
-
-video.addEventListener('timeupdate', function() {
-    if (playForward && video.currentTime >= duration) {
-        playForward = false;
-        reverseVideo();
-    } else if (!playForward && video.currentTime <= 0) {
-        playForward = true;
-        video.currentTime = 0;
-        video.play();
-    }
-});
-
-function reverseVideo() {
-    // video.pause();
-    let reverseInterval = setInterval(() => {
-        if (video.currentTime <= 0) {
-            clearInterval(reverseInterval);
-            playForward = true;
-            video.currentTime = 0;
-            video.play();
-        } else {
-            video.currentTime -= 0.5; // Adjust this value to change the reverse speed
+var vid = document.getElementById("vid");
+    vid.addEventListener("timeupdate", function () {
+        if(this.currentTime >= 21.0) {
+            this.currentTime = 0.0;
         }
-    }, 100); // This interval should match the reverse speed
-}
+    });
