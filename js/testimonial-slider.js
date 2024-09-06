43b1d3369c5d70ab896d1fd4c5b9d26d564a3f7c
Salsabeel Noor <salsabeelnoor@gmail.com>
2024-09-06 16:22:23 +0600

    color and banner updated

--- js/testimonial-slider.js
+++ js/testimonial-slider.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const testimonial = new Splide("#testimonialSlider", {
     type: 'loop',
     perPage: 3,
-    autoplay: true,
+    // autoplay: true,
     interval: 5000,
     start: 1,
     gap: '10px',
     focus: 'center',
     updateOnMove: true,
     pagination: false,
-    height: '450px',
+    // height: '450px',
     classes: {
       arrow: 'splide__arrow testimonial__arrow ',
     },
